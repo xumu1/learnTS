86134c123f019586bb1b71736afaedf0f3613e4f
xumu <932851779@qq.com>
2020-06-09 23:00:38 +0800

    设置自动编译成js

---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log("hello typescript")
+
+
+// ts 编译 es5
+
+var str: string = "hello ts"
+
+let num: any = "hello ts"
+
+let num1: any = 2
---
+++ js/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hello typescript");
+// ts 编译 es5
+var str = "hello ts";
+var num = "hello ts";
+var num1 = 2;
